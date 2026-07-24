a83caf03dfae5a8e8fbfbd5126eb0e99ae6ad2e1
Flora Rachel Katz-Andrade <165905891+FloraKatz27@users.noreply.github.com>
2026-07-24 23:19:51 +0100

    Implemented basic server socket and connection handling

--- src/main/java/library/LibraryServer.java
+++ src/main/java/library/LibraryServer.java
@@ -1,2 +1,26 @@
-package library;public class LibraryServer {
+package library;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class LibraryServer {
+    public static void main(String[] args) {
+        System.out.println("Starting library server...");
+
+        //ServerSocket serverSocket;
+        try {
+            ServerSocket serverSocket = new ServerSocket(5001);
+
+            System.out.println("Server is listening on port 5001.");
+
+            System.out.println("Waiting for a client...");
+
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println("A client has connected.");
+        } catch (IOException e) {
+            System.out.println("Server error: " + e.getMessage());
+        }
+    }
 }
